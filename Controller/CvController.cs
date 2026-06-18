0d65034169677e7073b6dc6be9a360b2916da1e5
Ankit Das <ankit.das@navsoft.in>
2026-06-18 13:06:29 +0530

    Updated

--- Controller/CvController.cs
+++ Controller/CvController.cs
@@ -9,7 +9,7 @@ public class CvController : ControllerBase
     private readonly string _baseUrl = "https://portfolio-6k0f.onrender.com";
 
     [HttpPost("upload")]
-    public async Task<IActionResult> UploadCv(IFormFile file)
+    public async Task<IActionResult> UploadCv([FromForm] IFormFile file)
     {
         if (file == null || file.Length == 0)
             return BadRequest("No file selected");
